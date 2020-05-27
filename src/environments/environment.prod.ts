677ad4e790dedaa732a4ffc27895dd95a279091f
Zeeshan Syed <zsyed89@gmail.com>
2020-05-27 09:01:01 -0400

    Setup basic app and configured firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBWqcPD_MSRbzSvCo3OLTP3qL7r7MzWszM",
+    authDomain: "kanban-2e249.firebaseapp.com",
+    databaseURL: "https://kanban-2e249.firebaseio.com",
+    projectId: "kanban-2e249",
+    storageBucket: "kanban-2e249.appspot.com",
+    messagingSenderId: "552636789909",
+    appId: "1:552636789909:web:f83eeb1deb5c68636a179c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBWqcPD_MSRbzSvCo3OLTP3qL7r7MzWszM",
+    authDomain: "kanban-2e249.firebaseapp.com",
+    databaseURL: "https://kanban-2e249.firebaseio.com",
+    projectId: "kanban-2e249",
+    storageBucket: "kanban-2e249.appspot.com",
+    messagingSenderId: "552636789909",
+    appId: "1:552636789909:web:f83eeb1deb5c68636a179c"
+  }
 };
 
 /*
